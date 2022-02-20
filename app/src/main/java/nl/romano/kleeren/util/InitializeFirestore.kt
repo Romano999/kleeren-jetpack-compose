19bedeb7db83059b06da93caba9b9b0b500ecff9
Romano999 <71527835+Romano999@users.noreply.github.com>
2022-02-20 20:08:53 +0100

    Util classes for firebase and testing made.

---
+++ app/src/main/java/nl/romano/kleeren/util/InitializeFirestore.kt
@@ -0,0 +1,69 @@
+package nl.romano.kleeren.util
+
+import com.google.firebase.firestore.FirebaseFirestore
+import nl.romano.kleeren.model.MProduct
+import nl.romano.kleeren.model.MUser
+
+/*
+    This class is made to check if the firebase connecting to this project
+    contains the necessary collections for this project to function correctly.
+ */
+class InitializeFirestore {
+    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val usersRef = db.collection("users")
+    private val salesRef = db.collection("sales")
+    private val productRef = db.collection("products")
+    private val arrivalsRef = db.collection("arrivals")
+    private val products: List<MProduct> = WebshopProducts.fillWebshopWithProducts()
+
+    fun initializeFirestoreCollectionsIfEmpty() {
+        initializeUsersCollectionIfEmpty()
+        initializeSalesCollectionIfEmpty()
+        initializeProductsCollectionIfEmpty()
+        initializeArrivalsCollectionIfEmpty()
+    }
+
+    private fun initializeUsersCollectionIfEmpty() {
+        val user = MUser(id = null, userId = "", displayName = "")
+
+        usersRef.get()
+            .addOnSuccessListener {
+                if (it.isEmpty) {
+                    db.collection("users").document().set(user.toMap())
+                }
+            }
+    }
+
+    private fun initializeSalesCollectionIfEmpty() {
+        salesRef.get()
+            .addOnSuccessListener {
+                if (it.isEmpty) {
+                    for (i in 0..4) {
+                        db.collection("sales").document().set(products[i].toMap())
+                    }
+                }
+            }
+    }
+
+    private fun initializeProductsCollectionIfEmpty() {
+        productRef.get()
+            .addOnSuccessListener {
+                if (it.isEmpty) {
+                    products.forEach { product ->
+                        db.collection("products").document().set(product.toMap())
+                    }
+                }
+            }
+    }
+
+    private fun initializeArrivalsCollectionIfEmpty() {
+        arrivalsRef.get()
+            .addOnSuccessListener {
+                if (it.isEmpty) {
+                    for (i in 4..8) {
+                        db.collection("arrivals").document().set(products[i].toMap())
+                    }
+                }
+            }
+    }
+}
