fb09994323baa77406331b689f6323cff5c23935
Romano999 <71527835+Romano999@users.noreply.github.com>
2022-02-22 21:47:19 +0100

    Search page layout done.

--- app/src/main/java/nl/romano/kleeren/screens/search/SearchScreen.kt
+++ app/src/main/java/nl/romano/kleeren/screens/search/SearchScreen.kt
@@ -1,26 +1,68 @@
 package nl.romano.kleeren.screens.search
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.* // ktlint-disable no-wildcard-imports
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import nl.romano.kleeren.component.BottomNavigationBar
+import nl.romano.kleeren.component.* // ktlint-disable no-wildcard-imports
+import nl.romano.kleeren.model.UserSearch
 
 @Composable
-fun SearchScreen(navController: NavController) {
-    // content()
-    Scaffold(bottomBar = {
-        BottomAppBar(
-            elevation = 5.dp
-        ) {
-            BottomNavigationBar(navController = navController)
+fun SearchScreen(
+    navController: NavController,
+    viewModel: SearchScreenViewModel = hiltViewModel()
+) {
+    val searchTerm = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    val userSearches = viewModel.userSearchList.collectAsState().value
+    val foundProducts = viewModel.foundProducts
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                elevation = 5.dp
+            ) {
+                BottomNavigationBar(navController = navController)
+            }
         }
-    }) {
+    ) {
         Column {
-            Text(text = "This is the search screen")
+            SearchInput(searchterm = searchTerm)
+            if (foundProducts.isEmpty() || searchTerm.value == "") {
+                UserSearchEntries(
+                    userSearches,
+                    onIconClick = { userSearch -> viewModel.deleteNote(userSearch) },
+                    onRowClick = { userSearch -> { } }
+                )
+            }
+
+            Text(text = "Results")
+            Button(onClick = {
+                viewModel.addUserSearch(UserSearch(searchTerm = searchTerm.value))
+                viewModel.searchProducts(UserSearch(searchTerm = searchTerm.value))
+            }) {
+                Text(text = "Search")
+            }
+            ProductItems(foundProducts)
         }
     }
+}
+
+@Composable
+fun SearchInput(searchterm: MutableState<String>) {
+    InputField(
+        valueState = searchterm,
+        labelText = "Search",
+        imageVector = Icons.Default.Search,
+    )
 }
---
+++ app/src/main/java/nl/romano/kleeren/screens/search/SearchScreenViewModel.kt
@@ -0,0 +1,44 @@
+package nl.romano.kleeren.screens.search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import nl.romano.kleeren.model.MProduct
+import nl.romano.kleeren.model.UserSearch
+import nl.romano.kleeren.repository.UserSearchRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchScreenViewModel @Inject constructor(private val repository: UserSearchRepository) : ViewModel() {
+    private val _userSearch = MutableStateFlow<List<UserSearch>>(emptyList())
+    val userSearchList = _userSearch.asStateFlow()
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val productRef = db.collection("products")
+    var foundProducts: MutableList<MProduct> = mutableListOf()
+
+    init {
+        viewModelScope.launch {
+            repository.deleteAll()
+            repository.getUserSearches().distinctUntilChanged().collect { listOfUserSearches ->
+                _userSearch.value = listOfUserSearches
+            }
+        }
+    }
+
+    fun searchProducts(userSearch: UserSearch) = viewModelScope.launch {
+        productRef.whereGreaterThanOrEqualTo("name", userSearch.searchTerm).get().addOnSuccessListener { products ->
+            products.documents.forEach { product ->
+                val foundProduct = MProduct.toProduct(product.data!!)
+                foundProducts.add(foundProduct)
+            }
+        }
+    }
+    fun addUserSearch(userSearch: UserSearch) = viewModelScope.launch { repository.insert(userSearch) }
+    fun deleteNote(userSearch: UserSearch) = viewModelScope.launch { repository.deleteNote(userSearch) }
+}
