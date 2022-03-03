b3820173967c763f0e24fed2cd824bcdc121443f
Romano999 <71527835+Romano999@users.noreply.github.com>
2022-03-03 21:19:26 +0100

    Favourite Screen layout done.

--- app/src/main/java/nl/romano/kleeren/screens/favourite/FavouriteScreen.kt
+++ app/src/main/java/nl/romano/kleeren/screens/favourite/FavouriteScreen.kt
@@ -1,17 +1,25 @@
 package nl.romano.kleeren.screens.favourite
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
 import androidx.compose.material.BottomAppBar
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import nl.romano.kleeren.component.BottomNavigationBar
+import nl.romano.kleeren.component.ProductItems
+import nl.romano.kleeren.model.MProduct
 
 @Composable
-fun FavouriteScreen(navController: NavController) {
-    // content()
+fun FavouriteScreen(
+    navController: NavController,
+    viewModel: FavouriteScreenViewModel = hiltViewModel()
+) {
+    val favouriteProducts = viewModel.favouriteProducts
     Scaffold(bottomBar = {
         BottomAppBar(
             elevation = 5.dp
@@ -20,7 +28,19 @@ fun FavouriteScreen(navController: NavController) {
         }
     }) {
         Column {
-            Text(text = "This is the favourite screen")
+            FavouriteList(favouriteProducts)
         }
     }
+}
+
+@Composable
+fun FavouriteList(products: List<MProduct>) {
+    Text(
+        text = "Your favorites",
+        fontSize = MaterialTheme.typography.caption.fontSize.times(3)
+    )
+    ProductItems(
+        products,
+        rowModifier = Modifier.padding(10.dp).fillMaxWidth()
+    )
 }
