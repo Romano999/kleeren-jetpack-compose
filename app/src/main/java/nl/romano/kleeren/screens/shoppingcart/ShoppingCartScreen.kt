aff14c9d34af3bba6455c5db85c6829ebfec6f33
Romano999 <71527835+Romano999@users.noreply.github.com>
2022-03-04 14:28:45 +0100

    Shopping Cart Screen layout done.

--- app/src/main/java/nl/romano/kleeren/screens/shoppingcart/ShoppingCartScreen.kt
+++ app/src/main/java/nl/romano/kleeren/screens/shoppingcart/ShoppingCartScreen.kt
@@ -1,17 +1,29 @@
 package nl.romano.kleeren.screens.shoppingcart
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.* // ktlint-disable no-wildcard-imports
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import nl.romano.kleeren.component.BottomNavigationBar
+import nl.romano.kleeren.component.ProductItems
+import nl.romano.kleeren.component.RoundButton
+import nl.romano.kleeren.model.MProduct
+import nl.romano.kleeren.ui.theme.Green
 
 @Composable
-fun ShoppingCartScreen(navController: NavController) {
-    // content()
+fun ShoppingCartScreen(
+    navController: NavController,
+    viewModel: ShoppingCartScreenViewModel = hiltViewModel()
+) {
+    val shoppingCartProducts = viewModel.shoppingCartProducts
+
     Scaffold(bottomBar = {
         BottomAppBar(
             elevation = 5.dp
@@ -20,7 +32,36 @@ fun ShoppingCartScreen(navController: NavController) {
         }
     }) {
         Column {
-            Text(text = "This is the shopping cart screen")
+            ShoppingCartList(shoppingCartProducts)
         }
+    }
+}
+
+@Composable
+fun ShoppingCartList(products: List<MProduct>) {
+    Text(
+        text = "Your shopping cart",
+        fontSize = MaterialTheme.typography.caption.fontSize.times(3)
+    )
+    ProductItems(
+        products,
+        rowModifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        RoundButton(
+            onClick = { },
+            text = "Place your order",
+            backgroundColor = Green,
+            modifier = Modifier
+                .fillMaxWidth(0.7f)
+                .padding(top = 20.dp)
+        )
     }
 }
